9d479d8ab5429c900a40c45845165a40d3aa5ee0
sssangho <sangho353@naver.com>
2025-07-29 16:46:48 +0900

    Upload:패키지-import

---
+++ 250729/src/sec06/exam02/mycompany/Car.java
@@ -0,0 +1,11 @@
+package sec06.exam02.mycompany;
+
+import sec06.exam02.hankook.Tire;
+import sec06.exam02.hyundai.Engine;
+
+public class Car {
+	Engine engine = new Engine();
+	Tire tire1;
+	sec06.exam02.kumho.Tire tire2 = new sec06.exam02.kumho.Tire();
+
+}
---
+++ 250729/src/sec06/exam02/mycompany/Member.java
@@ -0,0 +1,11 @@
+package sec06.exam02.mycompany;
+
+
+
+public class Member {
+	String name;
+	String phone;
+	String addr;
+	int sal;
+	
+}
