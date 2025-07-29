233dbc8bf6b5d1502a70686af802fe4f40cd061b
sssangho <sangho353@naver.com>
2025-07-29 16:45:10 +0900

    Upload:final필드와 상수

---
+++ 250729/src/sec04/exam04/Person.java
@@ -0,0 +1,17 @@
+package sec04.exam04;
+
+public class Person {
+	final String nation = "Korea";
+	final String ssn;
+	String name;
+	static final double PI = 3.141592;
+	
+	public Person(String ssn, String naem) {
+		super();
+		this.ssn = ssn;
+		this.name = name;
+	}
+	
+	
+
+}
---
+++ 250729/src/sec04/exam04/PersonEx.java
@@ -0,0 +1,11 @@
+package sec04.exam04;
+
+public class PersonEx {
+
+	public static void main(String[] args) {
+		Person person = new Person("123-1234", "홍길동");
+//		person.nation = "USA";
+//		person.ssn = "123-1234";
+	}
+
+}
