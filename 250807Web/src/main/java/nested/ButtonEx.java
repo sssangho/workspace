5069226f2cfe4894c52013f1587f30df5b330a86
sssangho <sangho353@naver.com>
2025-08-07 17:08:19 +0900

    Upload:중첩인터페이스

---
+++ 250807Web/src/main/java/nested/ButtonEx.java
@@ -0,0 +1,14 @@
+package nested;
+
+public class ButtonEx {
+
+	public static void main(String[] args) {
+		Button btn = new Button();
+		
+		btn.setOnClickListener(new CallListener());
+		btn.touch();
+		
+		btn.setOnClickListener(new MessageListener());
+		btn.touch();
+	}
+}
---
+++ 250807Web/src/main/java/nested/CallListener.java
@@ -0,0 +1,9 @@
+package nested;
+
+public class CallListener implements Button.OnClickListener  {
+
+	@Override
+	public void onClick() {
+		System.out.println("전화를 겁니다.");
+	}
+}
