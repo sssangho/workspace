5775f851078a9948348e379e1a33f0fbe35c3cf2
sssangho <sangho353@naver.com>
2025-08-13 12:49:00 +0900

    Upload:Servlet

---
+++ 250813Web/src/main/java/com/du/MoveTest.java
@@ -0,0 +1,55 @@
+package com.du;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MoveTest
+ */
+@WebServlet("/test")
+public class MoveTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MoveTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		System.out.println(uri);
+		String conPath = request.getContextPath();
+		System.out.println(conPath);
+		String com = uri.substring(conPath.length());
+		System.out.println(com);
+		
+		request.setAttribute("name", "홍길동");
+		if (com.equals("/test/a") || com.equals("/test")) {
+			request.getRequestDispatcher("a.jsp").forward(request, response);
+		} else if (com.equals("/test/b")) {
+			response.sendRedirect("b.jsp");
+		} else {
+			response.getWriter().println("Invalid Access!");
+			return;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
