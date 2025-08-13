7550b457350fa7bc37950839244c9767398cba0a
sssangho <sangho353@naver.com>
2025-08-13 11:05:23 +0900

    Upload:Thread

--- 250813Web/src/main/java/thread/ThreadEx4.java
+++ 250813Web/src/main/java/thread/ThreadEx4.java
@@ -5,22 +5,22 @@ import java.util.stream.IntStream;
 public class ThreadEx4 {
 
 	public static void main(String[] args) {
-//		Thread th = new Thread(new Runnable() {  // 익명의 구현객체
-//			
-//			@Override
-//			public void run() {
-//				for( int i =0; i<5; i++) {
-//					System.out.print("띵");
-//					try {
-//						Thread.sleep(500);
-//					} catch (InterruptedException e) {
-//						// TODO Auto-generated catch block
-//						e.printStackTrace();
-//					} // 0.5초 지연 500ms
-//				}
-//			}
-//		});
-//		th.start();
+		Thread th = new Thread(new Runnable() {  // 익명의 구현객체
+			
+			@Override
+			public void run() {
+				for( int i =0; i<5; i++) {
+					System.out.print("띵");
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} // 0.5초 지연 500ms
+				}
+			}
+		});
+		th.start();
 		
 		// 람다식으로 변경하기
 //		Thread th = new Thread(() -> {
@@ -37,19 +37,17 @@ public class ThreadEx4 {
 //		th.start();
 		
 		// 스트림으로 변경하기
-	
-		Thread th = new Thread(() -> {
-			IntStream.rangeClosed(1, 5).forEach((i) -> {
-				System.out.print("띵"+i);
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} // 0.5초 지연 500ms
-			});
-		});
-
+//		Thread th = new Thread(() -> {
+//			IntStream.rangeClosed(1, 5).forEach((i) -> {
+//				System.out.print("띵"+i);
+//				try {
+//					Thread.sleep(500);
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} // 0.5초 지연 500ms
+//			});
+//		});
 //		th.start();
 		
 		for( int i =0; i<5; i++) {
