a0118dee34523af11bcb9c8ee75448f326838259
sssangho <sangho353@naver.com>
2025-08-22 12:23:48 +0900

    Upload:Ajax 활용한 MySQL테이블가져오는웹서비스

---
+++ 250822Web/src/main/java/com/du/dto/Member.java
@@ -0,0 +1,15 @@
+package com.du.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Member {
+	
+	private String id;
+	private String pw;
+	private String name;
+}
