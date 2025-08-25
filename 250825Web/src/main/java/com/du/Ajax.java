061dc29b63c336a9ffcbadf19c56bd1f49e29dce
sssangho <sangho353@naver.com>
2025-08-25 19:25:53 +0900

    FIX:

---
+++ 250825Web/src/main/java/com/du/Ajax.java
@@ -0,0 +1,49 @@
+package com.du;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ajax
+ */
+@WebServlet("/Ajax")
+public class Ajax extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Ajax() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String str ="[{\"id\":100, \"title\":\"한식\"},\r\n"
+				+ "{\"id\":200, \"title\":\"양식\"},\r\n"
+				+ "{\"id\":300, \"title\":\"중식\"},\r\n"
+				+ "{\"id\":400, \"title\":\"일식\"}]";
+		out.print(str);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
